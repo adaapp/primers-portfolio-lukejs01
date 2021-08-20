22709afe7cf22cd168a19af087f6e45d16d0da30
lukejs01 <79159403+lukejs01@users.noreply.github.com>
2021-08-20 11:54:30 +0000

    primer 7 completed and commented

--- include/thursday.h
+++ include/thursday.h
@@ -1,18 +1,20 @@
 #include <thread>
 
-void SleeperTimer(int& seconds){
-  if(seconds == 0){
-    seconds = 10;
+void SleeperTimer(int seconds){
+  if(seconds == 0 ){
+    seconds = 10; // sets default to 10 if a value wasnt entered
   }
-  std::this_thread::sleep_for(std::chrono::milliseconds(seconds*1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(seconds*1000)); // calls the thread and sleeps for 10 seconds
+}
+void SleeperTimer(std::string value){ // for validation - if the argument is a stirng it calls the other function with a default of 10 seconds
+  SleeperTimer(10);
+  
 }
 
 void sleepTimer(void) {
-  std::string seconds;
-  std::cout << "Enter number of seconds to sleep: ";
-  std::getline(std::cin, seconds);
-  seconds = stoi(seconds);
-	SleeperTimer(seconds)
+  
+  
+	SleeperTimer(5);
 }
 
 
